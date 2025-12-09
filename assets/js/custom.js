ddaa3a6a15878e7837f83bea131aeaad109a4bec
148627654 <guoyuquan2013@126.com>
2025-12-09 22:13:07 +0800

    Updated version

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -100,7 +100,7 @@ $(document).ready(function(){
 		// iii. .brand-item (carousel)
 		
 			$('.brand-item').owlCarousel({
-				items:6,
+				items:4,//6
 				loop:true,
 				smartSpeed: 1000,
 				autoplay:true,
@@ -117,7 +117,7 @@ $(document).ready(function(){
 							items:3
 						},
 						1000:{
-							items:6
+							items:4//6
 						}
 					}
 				});
